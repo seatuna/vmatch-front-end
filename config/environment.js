956e74b918d1cd2c54f7491d1c45e3889742ef75
Celena T <chibiynm@gmail.com>
2016-04-07 21:39:17 -0400

    config environment

--- config/environment.js
+++ config/environment.js
@@ -40,8 +40,10 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/vmatch-front-end';
+    ENV.locationType = 'hash';
   }
+  
   ENV.baseURL = '/vmatch-front-end';
   ENV.locationType = 'hash';
   ENV.host = 'https://vmatch.herokuapp.com';
