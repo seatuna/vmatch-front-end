d9e86ae2137ec5d841b777d27db7df125031111c
Celena T <chibiynm@gmail.com>
2016-04-07 22:04:35 -0400

    Add host to adapter

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
+  host: 'https://vmatch.herokuapp.com/',
   auth: Ember.inject.service(),
 
   headers: Ember.computed('auth.credentials.token', {
