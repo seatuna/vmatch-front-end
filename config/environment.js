3293abafb652e59eb58a57c84d003a0d5afbadde
Celena T <chibiynm@gmail.com>
2016-04-19 13:50:53 -0400

    Edit production env

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/vmatch-front-end/';
+    ENV.locationType = 'hash';
   }
 
   // ENV.baseURL = '/vmatch-front-end';
